c9569493c697fcf9583e3507705696473a2dc904
Eduard Dumitrescul <dumitresculeduard@gmail.com>
2024-03-25 11:47:02 +0200

    lab 4

--- year2/alg-avans/laborator/main.cpp
+++ year2/alg-avans/laborator/main.cpp
@@ -1,61 +1,21 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <valarray>
 
-bool cmp(std::pair <int, int> a, std::pair <int, int> b) {
-    if(a.first == b.first)
-        return a.second > b.second;
-    return a.first < b.first;
-}
-
-void acoperireCuIntervale() {
-    int left, right;
-    std::cin >> left >> right;
-
-    int n;
-    std::cin >> n;
-    std::vector <std::pair <int, int>> v(n);
-    for(int i = 0; i < n; i ++) {
-        std::cin >> v[i].first >> v[i].second;
+void geneticiMutatie() {
+    int n, k;
+    std::string s;
+    std::cin >> n >> k >> s;
+    for(int i = 0; i < k; i ++) {
+        int pos;
+        std::cin >> pos;
+        s[pos] = 1 - s[pos] + 2 * '0';
     }
-
-    std::sort(v.begin(), v.end(), cmp);
-
-    std::vector <int> ans;
-    int curr = left;
-    int i = 0;
-
-    while(curr < right and i < v.size()) {
-        int max = -1e9, index;
-        while(v[i].first <= curr) {
-            if(max < v[i].second) {
-                max = v[i].second;
-                index = i;
-            }
-            i ++;
-        }
-
-        if(max == -1e9) {
-            std::cout << 0 << '\n';
-            return;
-        }
-
-        ans.push_back(index);
-        curr = max;
-    }
-
-    if(curr < right) {
-        std::cout << 0 << '\n';
-    }
-    else {
-        std::cout << ans.size() << '\n';
-        for(auto i: ans) {
-            std::cout << i + 1 << ' ';
-        }
-    }
+    std::cout << s << '\n';
 }
 
 int main() {
-    acoperireCuIntervale();
+    geneticiMutatie();
     return 0;
 }
