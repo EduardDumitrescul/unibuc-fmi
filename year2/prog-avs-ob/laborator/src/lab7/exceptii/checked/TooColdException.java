68657acaed2badf8d87eb9df1cdab190637be576
Eduard Dumitrescul <dumitresculeduard@gmail.com>
2024-04-19 10:05:23 +0300

    hw labs 6,7,8

---
+++ year2/prog-avs-ob/laborator/src/lab7/exceptii/checked/TooColdException.java
@@ -0,0 +1,13 @@
+package lab7.exceptii.checked;
+
+public class TooColdException extends Exception {
+
+    public TooColdException() {
+        super();
+    }
+
+    public TooColdException(String message) {
+        super(message);
+    }
+
+}
---
+++ year2/prog-avs-ob/laborator/src/lab7/exceptii/runtime/Parser.java
@@ -0,0 +1,7 @@
+package lab7.exceptii.runtime;
+
+public class Parser {
+    public static void main(String[] args) {
+        Integer.parseInt("abc"); // se arunca NumberFormatException
+    }
+}
